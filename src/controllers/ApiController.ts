8c94d923e218dbe75b5f3a7a225baa9fff02a522
Gabriel Cardoso <gabri3lcardoso07@gmail.com>
2022-09-06 04:30:48 -0300

    adicionando rotas para trailes oficiais e outros

--- src/controllers/ApiController.ts
+++ src/controllers/ApiController.ts
@@ -1,37 +1,49 @@
 import { Request, Response } from 'express';
-import apiNetflix from '../jsons/netflixYtApi.json'
-import apiPrimeVideo from '../jsons/primeVideoYtApi.json'
-import apiStartPlus from '../jsons/startPlusYtApi.json'
-import apiDisneyPlus from '../jsons/disneyPlusYtApi.json'
-import AllListApi from '../jsons/listAllApi.json'
-import all from '../jsons/allApi.json'
+import apiNetflix from '../jsons/netflixYtApi.json';
+import apiPrimeVideo from '../jsons/primeVideoYtApi.json';
+import apiStartPlus from '../jsons/startPlusYtApi.json';
+import apiDisneyPlus from '../jsons/disneyPlusYtApi.json';
+import trailers from '../jsons/trailersApi.json';
+import outros from '../jsons/outrosApi.json';
+import AllListApi from '../jsons/listAllApi.json';
+import all from '../jsons/allApi.json';
 
 export const lisAllApi = async (req: Request, res: Response)=>{
-    let result = AllListApi
-    res.json(result)
+    let result = AllListApi;
+    res.json(result);
 };
 
 export const listApiNetflix = async (req: Request, res: Response)=>{
-    let result = apiNetflix
-    res.json(result)
+    let result = apiNetflix;
+    res.json(result);
 };
 
 export const listApiPrimeVideo = async (req: Request, res: Response)=>{
-    let result = apiPrimeVideo
-    res.json(result)
+    let result = apiPrimeVideo;
+    res.json(result);
 };
 
 export const listApiStartPlus = async (req: Request, res: Response)=>{
-    let result = apiStartPlus
-    res.json(result)
+    let result = apiStartPlus;
+    res.json(result);
 };
 
 export const listApiDisneyPlus = async (req: Request, res: Response)=>{
-    let result = apiDisneyPlus
-    res.json(result)
+    let result = apiDisneyPlus;
+    res.json(result);
 };
 
 export const allApi = async (req: Request, res: Response)=>{
-    let result = all
-    res.json(result)
+    let result = all;
+    res.json(result);
+};
+
+export const allTrailers = async (req: Request, res: Response)=>{
+    let result = trailers;
+    res.json(result);
+};
+
+export const allOutros = async (req: Request, res: Response)=>{
+    let result = outros;
+    res.json(result);
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,8 @@ router.get('/api/netflix', ApiController.listApiNetflix);
 router.get('/api/primevideo', ApiController.listApiPrimeVideo);
 router.get('/api/starplus', ApiController.listApiStartPlus);
 router.get('/api/disneyplus', ApiController.listApiDisneyPlus);
+router.get('/api/trailers', ApiController.allTrailers);
+router.get('/api/outros', ApiController.allOutros);
 router.get('/api/all', ApiController.allApi);
 
 export default router;
